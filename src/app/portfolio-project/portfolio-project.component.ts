416eaf5fc4e237e723f086c2bc4a4df94d1ba42b
Markus Gede <markusgede93@gmail.com>
2023-11-10 11:39:42 +0100

    1011

--- src/app/portfolio-project/portfolio-project.component.ts
+++ src/app/portfolio-project/portfolio-project.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { PortfolioComponent } from '../portfolio/portfolio.component';
 
 @Component({
   selector: 'app-portfolio-project',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./portfolio-project.component.scss']
 })
 export class PortfolioProjectComponent {
+
+  @Input() projectIndex: number = 0;
+
+  public imagesComputers: string[] = [
+    '../../assets/img/projects/laptop_ohne_Hintergrund/pollo_loco_begin.svg',
+    '../../assets/img/projects/laptop_ohne_Hintergrund/join_begin.svg',
+    '../../assets/img/projects/laptop_ohne_Hintergrund/crm_begin.svg',
+    '../../assets/img/projects/laptop_ohne_Hintergrund/pokedex_begin.svg'
+  ];
 
 }
